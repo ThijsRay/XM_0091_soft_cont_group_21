ffaa05626031590d9582fe5e761e2cf459f4ccb5
Thijs Raymakers <thijs@raymakers.nl>
2023-01-24 14:00:34 +0100

    Use environmental variables for database config variables

--- src/backend/src/database.php
+++ src/backend/src/database.php
@@ -1,9 +1,12 @@
 <?php
 
 $pwd = getenv('POSTGRES_PASSWORD', true);
+$host = getenv('POSTGRES_HOST', true);
+$user = getenv('POSTGRES_USER', true);
+$db_name = getenv('POSTGRES_DB', true);
 
 global $db;
-$db = pg_connect("host=database dbname=postgres user=postgres password=$pwd")
+$db = pg_connect("host=$host dbname=$db_name user=$user password=$pwd")
   or die('Could not connect: ' . pg_last_error());
 
 $query = "CREATE TABLE IF NOT EXISTS images (
